5c984975a5b925c7788549b814bd95e7e9afcb2c
MartinLuzifer <oit@oren-sms.ru>
2024-11-11 23:49:29 +0500

    refc

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.mongo import MongoStorage
 from aiogram.enums import ParseMode
 from router import mainRouter
-from config import TELEGRAM_BOT_API_KEY
+from config import TELEGRAM_BOT_API_KEY, MONGODB_URL
 #####################################################################################################################
 
 
 async def main() -> None:
     bot = Bot(token=TELEGRAM_BOT_API_KEY, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    mongodb = MongoStorage.from_url(url='mongodb://root:example@172.30.250.200:27017') # mongodb://user:password@host:port
+    mongodb = MongoStorage.from_url(url=MONGODB_URL)
     dp = Dispatcher(storage=mongodb)
     dp.include_router(router=mainRouter)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())

40397ab3aec7f0780277c17ce2777d73412c47ce
MartinLuzifer <oit@oren-sms.ru>
2024-11-11 23:48:31 +0500

    config

--- config.example.py
+++ config.example.py
@@ -1,1 +1,16 @@
-TELEGRAM_BOT_API_KEY = ''
+from dotenv import dotenv_values
+
+values = dotenv_values('.env')
+
+
+TELEGRAM_BOT_API_KEY = values.get('TELEGRAM_BOT_API_KEY')
+
+MONGODB = { # do not change the sequence
+    'MONGODB_USER': values.get('MONGODB_USER'),
+    'MONGODB_PASS': values.get('MONGODB_PASS'),
+    'MONGODB_ADDR': values.get('MONGODB_ADDR'),
+    'MONGODB_PORT': values.get('MONGODB_PORT'),
+}
+MONGODB_URL = 'mongodb://{}:{}@{}:{}'.format(*MONGODB.values())
+
+print(TELEGRAM_BOT_API_KEY)
